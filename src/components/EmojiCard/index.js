6af1b3d79f3b5b908290bd4c0aa53292e8ca8354
McBk_Pro_Engine <dkria@dkrias-MacBook-Pro.local>
2019-02-02 00:01:37 -0800

    making the main page

--- src/components/EmojiCard/index.js
+++ src/components/EmojiCard/index.js
@@ -1,12 +1,1 @@
-import React from "react";
-import "./style.css";
-
-function EmojiCard(props) {
-    return (
-        <div className="card col-md-2 mt-4 mr-4 ml-4 mb-4">
-            <img className="card-img-top" alt={props.name} src={props.image} />
-        </div>
-    );
-}
-
-export default EmojiCard;
+export { default } from "./EmojiCard";
