9cbf6b8a7e4faa196bf3cc1a8f7efcc4427dcbc6
McBk_Pro_Engine <dkria@dkrias-MacBook-Pro.local>
2019-02-05 17:44:49 -0800

    Updates

---
+++ precache-manifest.7e6f318dc36d05fc2a30a67a43806559.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f7db49552bc07db73561",
+    "url": "/DKMS37/clickyGame/static/css/main.55b5e13d.chunk.css"
+  },
+  {
+    "revision": "f7db49552bc07db73561",
+    "url": "/DKMS37/clickyGame/static/js/main.f7db4955.chunk.js"
+  },
+  {
+    "revision": "01a0fc85a4c4072f14e5",
+    "url": "/DKMS37/clickyGame/static/js/1.01a0fc85.chunk.js"
+  },
+  {
+    "revision": "db40304abc16fd0e7bea",
+    "url": "/DKMS37/clickyGame/static/js/runtime~main.db40304a.js"
+  },
+  {
+    "revision": "9ca225837e0400447cf7c498c2e93542",
+    "url": "/DKMS37/clickyGame/index.html"
+  }
+];
